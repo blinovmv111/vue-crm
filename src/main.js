e0c438662b7ad6e6b3f1b152b13acb544fa3e135
Mihail Blinov <blinovmv111@gmail.com>
2020-12-21 09:58:09 +0300

    add dropdown USER, timer, action for button add record

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
-import router from './router'
+import router from './router.js'
 import store from './store'
+import dateFilter from '@/filters/date.filter'
 import 'materialize-css/dist/js/materialize.min.js'
 
 Vue.config.productionTip = false
+
+Vue.filter('date', dateFilter)
 
 new Vue({
   router,
